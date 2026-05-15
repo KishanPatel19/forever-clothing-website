e8ac17f560d05d97c62b50ad267855a3aa06760a
Kishan Patel <kvpatel6351665793@gmail.com>
2026-05-15 11:10:32 +0530

    `Added functionality to update cart quantity, added cart total and payment method selection to PlaceOrder page, and updated Cart page to display cart items and allow quantity updates`

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -18,7 +18,7 @@ function Product() {
         if(item._id === productId){
           setProductData(item);
           setImage(item.image[0])
-          console.log(item)
+          
           return null;
         }
       
